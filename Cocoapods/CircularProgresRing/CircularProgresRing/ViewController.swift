bba62df7f9faf8b11a52ac937a8934ea4d746b3e
RuslanWorkGit <lulka758@gmail.com>
2022-08-12 13:45:59 +0300

    Use cocoapods add UICircularProgressRIng and use his functional add two rings on controller

---
+++ Cocoapods/CircularProgresRing/CircularProgresRing/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  CircularProgresRing
+//
+//  Created by Ruslan Liulka on 12.08.2022.
+//
+
+import UIKit
+import UICircularProgressRing
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addRing()
+        addAnoutherRing()
+        // Do any additional setup after loading the view.
+    }
+    
+    func addRing() {
+        let info = CGRect(x: 100, y: 120, width: 150, height: 150)
+        let progressRing = UICircularProgressRing(frame: info)
+        progressRing.maxValue = 100
+        progressRing.innerRingColor = .red
+        
+        view.addSubview(progressRing)
+        
+        progressRing.value = 42
+        progressRing.animationTimingFunction = .linear
+        
+    }
+    
+    func addAnoutherRing() {
+        let ring = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: view.frame.width / 2, height: view.frame.width / 2)
+        let someRing = UICircularProgressRing(frame: ring)
+        someRing.maxValue = 8
+        someRing.innerRingColor = .orange
+        someRing.value = 2
+        someRing.animationTimingFunction = .default
+        someRing.center = view.center
+        
+        view.addSubview(someRing)
+    }
+
+
+}
+
